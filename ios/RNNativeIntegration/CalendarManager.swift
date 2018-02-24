5080d4a2f0c947301b997fce1328f303b5fbf10a
Josh Justice <jjustice@bignerdranch.com>
2018-02-24 06:26:50 -0500

    Add Swift class

---
+++ ios/RNNativeIntegration/CalendarManager.swift
@@ -0,0 +1,13 @@
+//
+//  CalendarManager.swift
+//  RNNativeIntegration
+//
+//  Created by Josh Justice on 2/24/18.
+//  Copyright © 2018 Facebook. All rights reserved.
+//
+
+import Foundation
+
+class CalendarManager: NSObject {
+
+}
