bfae54f2ef8758c9cc1b0f65b1f3bbd086dc2792
Josh Justice <jjustice@bignerdranch.com>
2018-02-24 06:36:41 -0500

    Create a Logger class to access from Swift

---
+++ ios/RNNativeIntegration/Logger.h
@@ -0,0 +1,15 @@
+//
+//  Logger.h
+//  RNNativeIntegration
+//
+//  Created by Josh Justice on 2/24/18.
+//  Copyright © 2018 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Logger : NSObject
+
++ (void)info:(NSString *)message;
+
+@end
