83002b623c9d6680ce1c0f09095f8f159090c19e
Josh Justice <jjustice@bignerdranch.com>
2018-03-03 12:04:00 -0500

    Remove unused stuff in App component

--- App.js
+++ App.js
@@ -7,20 +7,11 @@
 import React, { Component } from 'react';
 import {
   Button,
-  Platform,
-  StyleSheet,
   Text,
   View
 } from 'react-native';
 import {NativeModules} from 'react-native';
 var CalendarManager = NativeModules.CalendarManager;
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
 
 export default class App extends Component {
   async tryTheBridge() {
@@ -40,12 +31,6 @@ export default class App extends Component {
   }
 
   render() {
-    const region = {
-      latitude: 37.48,
-      longitude: -122.16,
-      latitudeDelta: 0.1,
-      longitudeDelta: 0.1,
-    };
     return (
       <View style={{ flex: 1 }}>
         <Text style={{ height: 50, paddingTop: 20 }}>Native Integration</Text>
@@ -58,22 +43,3 @@ export default class App extends Component {
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
